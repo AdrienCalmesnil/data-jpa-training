91c66fb715b0a6166a5aecb44fb8bcb828b86e67
Adrien Calmesnil <adcalmesnil@gmail.com>
2024-03-06 22:44:52 +0100

    first commit

---
+++ src/main/java/com/adrien/example/MyFirstClass.java
@@ -0,0 +1,15 @@
+package com.adrien.example;
+
+
+public class MyFirstClass {
+
+    private String myVar;
+
+    public MyFirstClass(String myVar) {
+        this.myVar = myVar;
+    }
+
+    public String hello(){
+        return "Hello from the FirstClass == myVar = " + myVar;
+    }
+}
