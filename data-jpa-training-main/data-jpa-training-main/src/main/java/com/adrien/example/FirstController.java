25198d75401800e82710837a5419b5b84270d31d
Adrien CALMESNIL <acalmesnil@consultants-solutec.fr>
2024-03-07 17:15:18 +0100

    clean repo data jpa

--- data-jpa-training-main/data-jpa-training-main/src/main/java/com/adrien/example/FirstController.java
+++ data-jpa-training-main/data-jpa-training-main/src/main/java/com/adrien/example/FirstController.java
@@ -14,24 +14,4 @@ public class FirstController {
     public String post(@RequestBody String message){
         return "Request accepted and message is : " + message;
     }
-
-    @PostMapping("/post-order")
-    public String post(@RequestBody Order order){
-        return "Request accepted and order is : " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String postRecord(@RequestBody Order order){
-        return "Request accepted and order is : " + order.toString();
-    }
-
-    @GetMapping("/hello/{user-name}")
-    public String pathVar(@PathVariable("user-name") String userName){
-        return "my value = " + userName;
-    }
-
-    @GetMapping("/hello")
-    public String paramVar(@RequestParam("user-name") String userName, @RequestParam("user-lastnam") String userLastname){
-        return "my value = " + userName + " " + userLastname;
-    }
 }
--- data-jpa-training-main/data-jpa-training-main/src/main/java/com/adrien/example/OrderRecord.java
+++
@@ -1,4 +0,0 @@
-package com.adrien.example;
-
-public record OrderRecord(String customerName, String productName, int quantity) {
-}
